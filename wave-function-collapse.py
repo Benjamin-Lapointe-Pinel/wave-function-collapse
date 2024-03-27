047ace19a56700f1c1a407ff74d80d03f12bfe81
Benjamin Lapointe-Pinel <benjamin.lapointe13@gmail.com>
2024-03-27 12:59:30 -0400

    fixed typo

--- wave-function-collapse.py
+++ wave-function-collapse.py
@@ -18,7 +18,6 @@ kernel = '''\
 
 kernel_line = kernel.replace('\n', '')
 rules = {char: {'weight': kernel_line.count(char), 'up': set(), 'down': set(), 'left': set(), 'right': set()} for char in set(kernel_line)}
-
 kernel_matrix = kernel.splitlines()
 for y in range(len(kernel_matrix)):
     for x in range(len(kernel_matrix[y])):
@@ -40,46 +39,46 @@ for char in rules:
         logging.debug(f"{key: <6} : {rules[char][key]}")
 
 
-# def get_least_entropy_coordinate(grid):
-#     entropy = [{'x': x, 'y': y, 'entropy': len(grid[x][y])} for x in range(len(grid)) for y in range(len(grid[x])) if len(grid[x][y]) > 1]
-#     min_entropy = min(entropy, key=lambda e: e['entropy'])['entropy']
-#     cells = [(e['x'], e['y']) for e in entropy if e['entropy'] <= min_entropy]
-#     return random.choice(cells)
+def get_least_entropy_coordinate(grid):
+    entropy = [{'x': x, 'y': y, 'entropy': len(grid[x][y])} for x in range(len(grid)) for y in range(len(grid[x])) if len(grid[x][y]) > 1]
+    min_entropy = min(entropy, key=lambda e: e['entropy'])['entropy']
+    cells = [(e['x'], e['y']) for e in entropy if e['entropy'] <= min_entropy]
+    return random.choice(cells)
 
 
-# def collapse(grid, x, y, stdscr):
-#     superposition = grid[x][y]
-#     weights = [rules[s] for s in superposition]
-#     char = random.choices(superposition, weights, k=1)[0]
-#     grid[x][y] = [char]
-#     try:
-#         stdscr.addstr(y, x, char)
-#     except curses.error:
-#         pass
-#     stdscr.refresh()
+def collapse(grid, x, y, stdscr):
+    superposition = grid[x][y]
+    weights = [rules[s]['weight'] for s in superposition]
+    char = random.choices(superposition, weights, k=1)[0]
+    grid[x][y] = [char]
+    try:
+        stdscr.addstr(y, x, char)
+    except curses.error:
+        pass
+    stdscr.refresh()
 
-#     # TODO collapse recursive
-
-
-# def main(stdscr):
-#     stdscr.clear()
-
-#     WIDTH = stdscr.getmaxyx()[1]
-#     HEIGHT = stdscr.getmaxyx()[0]
-#     WIDTH = 64
-#     HEIGHT = 32
-#     grid = [[list(rules.keys()) for _ in range(HEIGHT)] for _ in range(WIDTH)]
-
-#     for y in range(HEIGHT):
-#         for x in range(WIDTH):
-#             stdscr.insstr(y, x, '.')
-
-#     for _ in range(WIDTH * HEIGHT):
-#         x, y = get_least_entropy_coordinate(grid)
-#         collapse(grid, x, y, stdscr)
-
-#     curses.curs_set(0)
-#     stdscr.getkey()
+    # TODO collapse recursive
 
 
-# curses.wrapper(main)
+def main(stdscr):
+    stdscr.clear()
+
+    WIDTH = stdscr.getmaxyx()[1]
+    HEIGHT = stdscr.getmaxyx()[0]
+    WIDTH = 64
+    HEIGHT = 32
+    grid = [[list(rules.keys()) for _ in range(HEIGHT)] for _ in range(WIDTH)]
+
+    for y in range(HEIGHT):
+        for x in range(WIDTH):
+            stdscr.insstr(y, x, '.')
+
+    for _ in range(WIDTH * HEIGHT):
+        x, y = get_least_entropy_coordinate(grid)
+        collapse(grid, x, y, stdscr)
+
+    curses.curs_set(0)
+    stdscr.getkey()
+
+
+curses.wrapper(main)
