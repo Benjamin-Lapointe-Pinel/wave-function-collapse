176e9886ba779c9b624389acbc4e66a3377417a5
Benjamin Lapointe-Pinel <benjamin.lapointe13@gmail.com>
2024-03-28 13:28:52 -0400

    begining of collapse

--- wave-function-collapse.py
+++ wave-function-collapse.py
@@ -3,6 +3,7 @@
 import logging
 import random
 import curses
+from collections import deque
 
 kernel = '''\
    ╻ ╻ ╻   
@@ -39,6 +40,20 @@ for char in rules:
         logging.debug(f"{key: <6} : {rules[char][key]}")
 
 
+def draw(grid, x, y, stdscr):
+    cell = grid[x][y]
+    if len(cell) == 0:
+        char = '!'
+    elif len(cell) == 1:
+        char = cell[0]
+    if char:
+        try:
+            stdscr.addstr(y, x, char)
+        except curses.error:
+            pass
+        stdscr.refresh()
+
+
 def get_least_entropy_coordinate(grid):
     entropy = [{'x': x, 'y': y, 'entropy': len(grid[x][y])} for x in range(len(grid)) for y in range(len(grid[x])) if len(grid[x][y]) > 1]
     min_entropy = min(entropy, key=lambda e: e['entropy'])['entropy']
@@ -46,18 +61,34 @@ def get_least_entropy_coordinate(grid):
     return random.choice(cells)
 
 
+def get_neighbors(grid, x, y):
+    coordinates = []
+    if x - 1 >= 0:
+        coordinates.append((x - 1, y))
+    if x + 1 < len(grid):
+        coordinates.append((x + 1, y))
+    if y - 1 >= 0:
+        coordinates.append((x, y - 1))
+    if y + 1 < len(grid[x]):
+        coordinates.append((x, y + 1))
+    return coordinates
+
+
 def collapse(grid, x, y, stdscr):
     superposition = grid[x][y]
     weights = [rules[s]['weight'] for s in superposition]
     char = random.choices(superposition, weights, k=1)[0]
     grid[x][y] = [char]
-    try:
-        stdscr.addstr(y, x, char)
-    except curses.error:
-        pass
-    stdscr.refresh()
+    draw(grid, x, y, stdscr)
 
-    # TODO collapse recursive
+    update(grid, deque(get_neighbors(grid, x, y)))
+
+
+def update(grid, queue):
+    if queue:
+        coordinate = queue.pop()
+
+        update(grid, queue)
 
 
 def main(stdscr):
